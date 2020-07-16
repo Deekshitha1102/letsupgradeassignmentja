f4c26140b55ee13795b3631c3bfc9427e4741540
Deekshitha1102 <61546523+Deekshitha1102@users.noreply.github.com>
2020-07-16 13:48:05 +0530

    Assignment day 2

---
+++ assignmentday2/day2.js
@@ -0,0 +1,46 @@
+//let name = prompt("what is your name ?");
+//console.log(name);
+
+//methods of string and array
+
+var num_ar = [20,30,40,50];
+var str_ar = ["megha","gopal"];
+
+num_ar.push(70);
+
+console.log("After push" + num_ar);
+console.log(num_ar.unshift(20,30));
+console.log(num_ar.shift(20,30));
+console.log(num_ar.pop());
+console.log(num_ar.splice(1, 2, "dad", "mom", "dee")); 
+
+
+
+
+
+let text = "An apple keepts doctor away";
+
+console.log(text);
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.indexOf("apple"));
+console.log(text.lastIndexOf("apple"));
+console.log(text.search("apple"));
+console.log(text[9]);
+console.log(text.charAt('8'));
+console.log(text.slice(4,9));
+console.log(text.slice(-4,-9));
+console.log(text.slice(9));
+console.log(text.substring(4,9));
+console.log(text.substr(5,9));
+console.log(text.substr(-4));
+console.log(text.replace("apple","pineapple"));
+console.log(text.charCodeAt(7));
+
+
+
+
+
+
+
